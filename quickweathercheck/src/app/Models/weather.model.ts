d856fb95b15331cf083f47c50649cb56cce48f28
lifekhomola <khomolavhutshilo5@gmail.com>
2022-05-18 10:06:43 +0200

    weather interfaces

---
+++ quickweathercheck/src/app/Models/weather.model.ts
@@ -0,0 +1,41 @@
+export interface root{
+
+    coord: Coord,
+    weather: Weather,
+    base: string,
+    main:Main,
+    visibility: number,
+    wind:Wind,
+    clouds:Clouds,
+    dt: number,
+    sys:Sys,
+    timezone:number,
+    id:number,
+    name:string,
+    cod:number,
+}
+
+export interface Coord{
+    lon:number,
+    lat:number,
+}
+export interface Weather{
+    id:number,
+    main:string,
+    description:string,
+    icon:string,
+
+}
+export interface Main{
+    temp: number;
+    feelslike:number;
+    temp_min:number;
+    tenp_max: number;
+    pressure:number;
+    humidity:number;
+}
+
+export interface Wind{
+    speed:number;
+    deg:number
+}
