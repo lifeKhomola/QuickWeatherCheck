2bae9145ef4783ccc2774b577f27c699236cc03d
lifekhomola <khomolavhutshilo5@gmail.com>
2022-05-18 09:57:42 +0200

    services and models

--- quickweathercheck/src/app/app.component.ts
+++ quickweathercheck/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quickweathercheck';
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  cityName: string = 'Wellington';
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+  this.getWeatherData(this.cityName);
+  this.cityName = '';
+  }
+
+  onSubmit() {
+  this.getWeatherData(this.cityName);
+  this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+  this.weatherService.getWeatherData(cityName)
+  .subscribe({
+      next: (response) => {
+      this.weatherData = response;
+      console.log(response);
+      }
+  });
+  }
 }
